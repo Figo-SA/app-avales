2bc831054fcbe57cd56e2f4453239ec719e1bfd0
Chavi <xavierchavez916@gmail.com>
2025-09-23 14:31:16 -0500

    Update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,51 +2,44 @@
 import { useColorScheme } from "react-native"; // Hook nativo
 
 // Navegación
-import {
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationDefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
 
 // Gesture Handler
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Paper UI + Adaptador de temas
-import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  PaperProvider,
-  adaptNavigationTheme,
-} from "react-native-paper";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 
 // Iconos
 import { Feather, FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 // Utilidades de terceros
-import merge from "deepmerge";
 import { Toasts } from "@backpackapp-io/react-native-toast";
-
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 // Constantes del proyecto
 import { Colors } from "../constants/Colors";
 
-const customDarkTheme = { ...MD3DarkTheme, colors: Colors.dark };
-const customLightTheme = { ...MD3LightTheme, colors: Colors.light };
-
-const { LightTheme, DarkTheme } = adaptNavigationTheme({
-  reactNavigationLight: NavigationDefaultTheme,
-  reactNavigationDark: NavigationDarkTheme,
-});
-
-const CombinedDefaultTheme = merge(LightTheme, customLightTheme);
-const CombinedDarkTheme = merge(DarkTheme, customDarkTheme);
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme(); // Usar el hook nativo
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
 
+  const customDarkTheme = {
+    ...MD3DarkTheme,
+    colors: {
+      ...theme.dark,
+      ...Colors.dark,
+    },
+  };
+
+  const customLightTheme = {
+    ...MD3LightTheme,
+    colors: {
+      ...theme.light,
+      ...Colors.light,
+    },
+  };
   const paperTheme =
-    colorScheme === "dark" ? CombinedDarkTheme : CombinedDefaultTheme;
-
+    colorScheme === "dark" ? customDarkTheme : customLightTheme;
   return (
     <PaperProvider
       theme={paperTheme}
@@ -84,12 +77,10 @@ export default function RootLayout() {
         },
       }}
     >
-      <ThemeProvider value={paperTheme}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <Stack screenOptions={{ headerShown: false }} />
-          <Toasts />
-        </GestureHandlerRootView>
-      </ThemeProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }} />
+        <Toasts />
+      </GestureHandlerRootView>
     </PaperProvider>
   );
 }
